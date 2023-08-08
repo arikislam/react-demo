7b9010eca4aacf833f78da487589b9f025256e4b
S M Ariq Islam <arikislam321@gmail.com>
2023-08-08 14:57:46 +0600

    Impliment dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import NavBar from "./COMPONENTS/NavBar";
 
 function App() {
     return (
-        <Grid templateAreas={`"nav nav" "aside main"`}>
-            <GridItem area="nav" >
+        <Grid templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+        }}>
+            <GridItem area="nav">
                 <NavBar/>
             </GridItem>
 
-            <GridItem area="aside" bg='gold'>
-                Aside
-            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside" bg='gold'>
+                    Aside
+                </GridItem>
+            </Show>
+
             <GridItem area="main" bg='yellow'>
                 Main
             </GridItem>
