031ef80059ec5fa5e4e4704f6739e350aa89b84e
S M Ariq Islam <arikislam321@gmail.com>
2023-08-09 02:04:14 +0600

    Remove duplicated syles

--- src/COMPONENTS/GameGrid.tsx
+++ src/COMPONENTS/GameGrid.tsx
@@ -2,20 +2,27 @@ import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import {GameCardSkeleton} from "./GameCardSkeleton";
+import {GameCardContainer} from "./GameCardContainer";
 
 
 const GameGrid = () => {
 
     const {games, error, isLoading} = useGames();
-    const skeletons = [1,2,3,4,5,6,7,8, 9, 10];
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid spacing={10} columns={{sm: 1, md:2, lg:3, xl:5}} padding="10px">
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+            <SimpleGrid spacing={10} columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding="10px">
+                {isLoading && skeletons.map(skeleton => (
+                    <GameCardContainer>
+                        <GameCardSkeleton key={skeleton}/>
+                    </GameCardContainer>
+                ))}
 
                 {games.map(game => (
-                    <GameCard game={game} key={game.id}/>
+                    <GameCardContainer>
+                        <GameCard game={game} key={game.id}/>
+                    </GameCardContainer>
                 ))}
             </SimpleGrid>
         </>
