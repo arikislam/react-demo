492a07b4b3f3dbe63e8cb1a000b688d50d8cc0cd
S M Ariq Islam <arikislam321@gmail.com>
2023-08-08 18:38:11 +0600

    Get optimized images

---
+++ src/services/card-image-url.ts
@@ -0,0 +1,7 @@
+const getCroppedImageUrl = (url: string, width: number = 600, height: number =400):string => {
+    let target = 'media/';
+    let index = url.indexOf(target)+target.length;
+    return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
+}
+
+export default getCroppedImageUrl;
