9714e89c13a2451ad6e89972388e368520abd61c
S M Ariq Islam <arikislam321@gmail.com>
2023-08-09 14:38:13 +0600

    Showing spinner and handle errors

--- src/COMPONENTS/GenreList.tsx
+++ src/COMPONENTS/GenreList.tsx
@@ -1,10 +1,14 @@
-import useData from "../hooks/useData";
 import useGenres from "../hooks/useGenres";
-import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import {HStack, Image, List, ListItem, Spinner, Text} from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/card-image-url";
 
 export const GenreList = () => {
-    const {data} = useGenres();
+    const {data, isLoading, error} = useGenres();
+
+    if (isLoading) return <Spinner/>;
+
+    if (error) return null;
+
     return (
         <List>
             {data.map(genre => (
