c426b3ad37867545aaa331987d3f8ace5b6028ad
S M Ariq Islam <arikislam321@gmail.com>
2023-08-10 13:18:50 +0600

    Some minor fixes

--- src/COMPONENTS/PlatformList.tsx
+++ src/COMPONENTS/PlatformList.tsx
@@ -3,7 +3,7 @@ import {SiNintendo} from 'react-icons/si';
 import {MdPhoneIphone} from 'react-icons/md';
 import {BsGlobe} from "react-icons/bs";
 
-import {Platform} from "../hooks/useGames";
+import {Platform} from "../hooks/usePlatforms";
 import {HStack, Icon, Text} from "@chakra-ui/react";
 import {IconType} from "react-icons";
 
